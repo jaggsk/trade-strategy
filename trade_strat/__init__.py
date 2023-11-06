7589d93a3e5ee4227c07340efe72eb305fbe29e7
jaggsk <kevin.jaggs@gmail.com>
2023-11-06 23:59:36 +0000

    New Requirements

--- trade_strat/__init__.py
+++ trade_strat/__init__.py
@@ -1,10 +1,3 @@
-#import trade_strat.indicators
-#import trade_strat.scans
-#import trade_strat.signals
-#from .indicators import *
-#from .scans import *
-#from .signals import *
-
 from trade_strat.core import *
 
-__version__ = "0.0.2"
+__version__ = "0.0.4"
